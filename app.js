11c2cd2635b9479b9d3a8e37e8523bd32dccfe8a
Bong <2021bongwonny@gmail.com>
2023-04-03 23:44:57 +0900

    🗑️remove : 불필요한 파일 및 패키지 삭제
    - fetch.js 삭제
    - body-parser 패키지 삭제

--- app.js
+++ app.js
@@ -11,11 +11,9 @@ const feeds = [
 
 const http = require('http');
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
--- fetch.js
+++
@@ -1,5 +0,0 @@
-fetch('http://localhost:8000/user_posting?searchuser=2021bong')
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-  });
