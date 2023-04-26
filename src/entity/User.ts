c58a2bd27452feb90e6863cab3ae80a77fcdab9d
Bong <2021bongwonny@gmail.com>
2023-04-26 22:50:44 +0900

    ✨: 유저 목록 조회, 유저 추가 api 추가
    - 조회(GET /users), 추가(POST /user)
    - body-parser 추가 및 적용

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,36 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number
+  @Column()
+  name: string;
 
-    @Column()
-    firstName: string
+  @Column()
+  age: number;
 
-    @Column()
-    lastName: string
+  @Column()
+  sex: string;
 
-    @Column()
-    age: number
+  @Column()
+  phone_number: string;
 
+  @Column()
+  symptom: string;
+
+  @Column()
+  reservation_time: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,22 @@
 import { AppDataSource } from './data-source';
-import { User } from './entity/User';
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
+import router from './api';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(router);
 
 AppDataSource.initialize()
   .then(async () => {
-    console.log('Inserting a new user into the database...');
-    const user = new User();
-    user.firstName = 'wonny';
-    user.lastName = 'bong';
-    user.age = 100;
-    await AppDataSource.manager.save(user);
-    console.log('Saved a new user with id: ' + user.id);
-
-    console.log('Loading users from the database...');
-    const users = await AppDataSource.manager.find(User);
-    console.log('Loaded users: ', users);
-
-    console.log(
-      'Here you can setup and run express / fastify / any other framework.'
-    );
+    console.log('Success Connect DB!');
   })
   .catch((error) => console.log(error));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-});
-
-app.get('/ping', (req: Request, res: Response) => {
-  res.send('pong');
 });
